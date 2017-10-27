224d32f54e7ffc920d231ea05504e8fdf433cf36
Junoriaz <2366372966@qq.com>
2017-10-27 10:44:38 +0800

    change

--- pubset/set.js
+++ pubset/set.js
@@ -1,4 +1,3 @@
-var imgAddress='http://192.168.0.49//image/';
-// var imgAddress='http://a.vrol.cn//image//';
+//var imgAddress='http://192.168.0.49//image/';
+var imgAddress='http://a.vrol.cn//image//';
 var debug=true;
-// console.log(111);
